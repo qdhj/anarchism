4dd00e0b8f64e0748e8e8bd80cf31262fba0ce01
Jazmin Kamarina <122599546+qdhj@users.noreply.github.com>
2023-05-03 18:12:41 +0300

    Add files via upload

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,11 @@
-import { useEffect } from "react"
-import { useRouter } from "next/router"
-
 export default function Custom404() {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.replace("/")
-  })
-
-  return null
+  return null;
 }
+
+export const getStaticProps = () => {
+  return {
+    redirect: {
+      destination: '/',
+    },
+  };
+};
